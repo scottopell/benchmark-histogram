2af17afbeae3db55388ead8f1fd5e498af50a0b8
Scott Opell <me@scottopell.com>
2025-01-21 18:32:25 -0500

    Does this work?

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 /** @type {import('vite').UserConfig} */
 export default defineConfig({
   plugins: [react()],
-  base: './'
+  base: './benchmark-histogram/'
 })
