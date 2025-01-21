086c781e3daa40531895f144e38234793978c395
Scott Opell <me@scottopell.com>
2025-01-20 21:33:18 -0500

    rename and screenshot

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import BenchmarkHistogram from './components/BenchmarkHistogram'
+import BenchmarkHistogram from './components/BenchmarkHistogram.tsx'
 
 function App() {
- return (
-   <div className="min-h-screen bg-gray-50 p-8">
-     <BenchmarkHistogram />
-   </div>
- )
+  return (
+    <div className="min-h-screen bg-gray-50 p-8">
+      <BenchmarkHistogram />
+    </div>
+  )
 }
 
 export default App
